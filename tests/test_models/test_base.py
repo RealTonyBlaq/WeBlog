a2519665933be9d7cc65623e95480ad90ea06f8f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-12 21:51:31 +0100

    update: Added test for pep8

--- tests/test_models/test_base.py
+++ tests/test_models/test_base.py
@@ -2,10 +2,61 @@
 """ BaseClass Test Suite """
 
 import unittest
+import inspect
+import models.base
 from models.base import BaseClass
+import pep8 as pycodestyle
 from sqlalchemy.orm.exc import UnmappedInstanceError
 from datetime import datetime
 import time
+
+
+module_doc = models.base.__doc__
+
+
+class TestBaseClassDocs(unittest.TestCase):
+    """Tests to check the documentation and style of BaseModel class"""
+
+    @classmethod
+    def setUpClass(self):
+        """Set up for docstring tests"""
+        self.base_funcs = inspect.getmembers(BaseClass, inspect.isfunction)
+
+    def test_pep8_conformance(self):
+        """Test that models/base.py conforms to PEP8."""
+        for path in ['models/base.py',
+                     'tests/test_models/test_base.py']:
+            with self.subTest(path=path):
+                errors = pycodestyle.Checker(path).check_all()
+                self.assertEqual(errors, 0)
+
+    def test_module_docstring(self):
+        """Test for the existence of module docstring"""
+        self.assertIsNot(module_doc, None,
+                         "base_model.py needs a docstring")
+        self.assertTrue(len(module_doc) > 1,
+                        "base_model.py needs a docstring")
+
+    def test_class_docstring(self):
+        """Test for the BaseModel class docstring"""
+        self.assertIsNot(BaseClass.__doc__, None,
+                         "BaseModel class needs a docstring")
+        self.assertTrue(len(BaseClass.__doc__) >= 1,
+                        "BaseModel class needs a docstring")
+
+    def test_func_docstrings(self):
+        """Test for the presence of docstrings in BaseModel methods"""
+        for func in self.base_funcs:
+            with self.subTest(function=func):
+                self.assertIsNot(
+                    func[1].__doc__,
+                    None,
+                    "{:s} method needs a docstring".format(func[0])
+                )
+                self.assertTrue(
+                    len(func[1].__doc__) > 1,
+                    "{:s} method needs a docstring".format(func[0])
+                )
 
 
 class TestBaseClassAttributes(unittest.TestCase):
