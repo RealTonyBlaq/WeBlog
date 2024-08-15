805d13ad89814b50e8716fa64de400f4c23ef3d0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-15 08:49:39 +0100

    user Model

---
+++ models/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" The User Model inherits from BaseClass """
+
+from datetime import datetime
+from sqlalchemy import Column, String, Boolean, DateTime
+from sqlalchemy.orm import relationship
+from models.associations import user_post
+from models.base import BaseClass, Base
+from models.post import Post
+
+
+class User(BaseClass, Base):
+    """ Defining the User Model class attributes """
+    __tablename__ = 'users'
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(60), nullable=False, unique=True)
+    last_login = Column(DateTime, default=datetime.now())
+    password = Column(String(60), nullable=False)
+    is_email_verified = Column(Boolean, default=False)
+    username = Column(String(25), nullable=False, unique=True)
+    articles = relationship('Post', backref='users',
+                            cascade='all, delete, delete-orphan',
+                            overlaps="likes,posts")
+    bookmarks = relationship('Post', secondary=user_post,
+                             back_populates='users', overlaps="bookmarked_by")
+    interested_subjects = relationship('Tag', backref='users')
