32072714a09d3602ad4a4a2cf1abee48f71a3531
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-15 08:50:49 +0100

    updated

--- models/post.py
+++ models/post.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ The Post Model """
 
+from models.associations import user_post
 from models.base import BaseClass, Base
 from models.tag import Tag
-from sqlalchemy import String, Column, ForeignKey, Integer
+from sqlalchemy import String, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -13,7 +14,10 @@ class Post(BaseClass, Base):
     title = Column(String(256), nullable=False)
     body = Column(String(1024), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    likes = Column(Integer, default=0)
-    bookmarks = Column(Integer, default=0)
-    tags = relationship('Tag', backref='post',
-                        cascade='all, delete, delete-orphan')
+    likes = relationship('User', backref='posts', overlaps="likes,posts")
+    bookmarked_by = relationship('User',
+                                 secondary=user_post,
+                                 back_populates='posts')
+    tags = relationship('Tag', backref='posts')
+    comments = relationship('Comment', backref='posts',
+                            cascade='all, delete, delete-orphan')
