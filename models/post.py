76afbdfbf5110af808480d6d2a4d04d76971846c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-16 22:51:30 +0100

    added header_url, many-to-many tag rltnship

--- models/post.py
+++ models/post.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ The Post Model """
 
-from models.associations import user_post
+from models.associations import user_post, post_tag
 from models.base import BaseClass, Base
 from models.tag import Tag
 from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.orm import relationship
 
 
@@ -12,12 +13,14 @@ class Post(BaseClass, Base):
     """ Defining the Post Class """
     __tablename__ = 'posts'
     title = Column(String(256), nullable=False)
-    body = Column(String(1024), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    body = Column(LONGTEXT, nullable=False)
+    author_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    header_url = Column(String(100), nullable=True)
     likes = relationship('User', backref='posts', overlaps="likes,posts")
     bookmarked_by = relationship('User',
                                  secondary=user_post,
-                                 back_populates='posts')
-    tags = relationship('Tag', backref='posts')
+                                 back_populates='bookmarks')
+    tags = relationship('Tag', secondary=post_tag,
+                        back_populates='posts')
     comments = relationship('Comment', backref='posts',
                             cascade='all, delete, delete-orphan')
