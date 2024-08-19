ca36ce9377045bdbdb5d5f37809946c7fe8c83b3
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-19 01:58:59 +0100

    update

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Test Suite for the User Model """
 
-from utils.database import DB
+from utils.database import DB, Session
 from datetime import datetime
 import inspect
 from models.user import User
@@ -62,61 +62,56 @@ class TestUserDocs(unittest.TestCase):
 
 class TestUser(unittest.TestCase):
     """ Tests the functionality of the User class """
-    def test_initialization_without_params(self):
-        """ Tests the User Object creation """
-        data = {
+    DATA = {
             'first_name': 'admin',
             'last_name': 'tester',
             'email': 'admin1@example.com',
             'username': 'admin_tester2',
             'password': 'test_password'
-        }
-        user = User(**data)
-        self.assertIsNotNone(user, 'No kwargs was passed as parameter')
+    }
+
+    def setUp(self):
+        """Set up test data before each test"""
+        self.data = self.DATA.copy()
+
+    def test_initialization(self):
+        """ Tests the User Object creation """
+        user = User(**self.data)
+        self.assertIsNotNone(user, 'User object was not created')
+
+        with self.assertRaises(ValueError, msg='No kwargs were passed'):
+            User()
 
     def test_id(self):
         """ Tests the id attribute """
-        data = {
-            'id': None,
-            'first_name': 'admin',
-            'last_name': 'tester',
-            'email': 'admin1@example.com',
-            'username': 'admin_tester2',
-            'password': 'test_password'
-        }
-        new_user = User(**data)
-        new_user_2 = User(**data)
+        self.data['id'] = None
+
+        new_user = User(**self.data)
+        new_user_2 = User(**self.data)
 
         self.assertIsNotNone(new_user.id, 'id cannot be None')
         self.assertTrue(type(new_user.id) is str,
                         'id should be a string object')
         self.assertTrue(new_user.id != new_user_2.id, 'ids must be unique')
 
-        data['id'] = '123456789'
-        new_user_3 = User(**data)
+        self.data['id'] = '123456789'
+        new_user_3 = User(**self.data)
         self.assertFalse(new_user_3.id == '123456789',
                          'id must be auto-generated and unique')
 
     def test_created_at(self):
         """ Tests the created_at attribute """
-        data = {
-            'first_name': 'admin',
-            'last_name': 'tester',
-            'email': 'admin1@example.com',
-            'username': 'admin_tester2',
-            'password': 'test_password',
-            'created_at': 'Today'
-        }
-        new_user = User(**data)
+        self.data['created_at'] = 'Today'
+        new_user = User(**self.data)
 
         self.assertFalse(new_user.created_at == 'Today',
                          'created_at should be auto-generated')
         self.assertTrue(isinstance(new_user.created_at, datetime),
                         'created_at should be a datetime object')
 
-        time.sleep(4)
+        time.sleep(2)
 
-        new_user2 = User(**data)
+        new_user2 = User(**self.data)
         self.assertTrue(new_user.created_at != new_user2.created_at,
                         'Time created should be different')
         self.assertTrue(new_user.created_at < new_user2.created_at,
@@ -124,16 +119,9 @@ class TestUser(unittest.TestCase):
 
     def test_updated_at(self):
         """ Tests the updated_at attribute """
-        data = {
-            'first_name': 'admin',
-            'last_name': 'tester',
-            'email': 'admin@example.com',
-            'username': 'admin_tester',
-            'password': 'test_password',
-            'updated_at': 'Today'
-        }
+        self.data['updated_at'] = 'Today'
 
-        new_user = User(**data)
+        new_user = User(**self.data)
 
         self.assertTrue(new_user.updated_at != 'Today',
                         'updated_at should be auto-generated')
@@ -142,7 +130,7 @@ class TestUser(unittest.TestCase):
 
         time.sleep(2)
 
-        new_user2 = User(**data)
+        new_user2 = User(**self.data)
         before = new_user2.updated_at
         self.assertTrue(new_user.updated_at < new_user2.updated_at,
                         'updated_at should be different for the two objects')
@@ -158,14 +146,7 @@ class TestUser(unittest.TestCase):
 
     def test_first_name(self):
         """ Tests the first_name attribute """
-        data = {
-            'first_name': 'admin',
-            'last_name': 'tester',
-            'email': 'admin1@example.com',
-            'username': 'admin_tester2',
-            'password': 'test_password'
-        }
-        new_user = User(**data)
+        new_user = User(**self.data)
         self.assertTrue(new_user.first_name == 'admin',
                         'first_name should be admin')
         self.assertTrue(type(new_user.first_name) is str,
@@ -173,39 +154,52 @@ class TestUser(unittest.TestCase):
         del new_user
 
         # Tests User creation without a first_name
-        del data['first_name']
+        del self.data['first_name']
         with self.assertRaises(ValueError, msg='first_name missing'):
-            User(**data)
+            User(**self.data)
 
         # Tests User creation with first_name as an integer
-        data['first_name'] = 200
+        self.data['first_name'] = 200
         with self.assertRaises(TypeError,
                                msg='first_name must be a string'):
-            User(**data)
+            User(**self.data)
 
     def test_last_name(self):
         """ Tests the last_name attribute """
-        data = {
-            'first_name': 'admin',
-            'last_name': 'tester',
-            'email': 'admin1@example.com',
-            'username': 'admin_tester2',
-            'password': 'test_password'
-        }
-        new_user = User(**data)
+        new_user = User(**self.data)
         self.assertTrue(new_user.last_name == 'tester',
-                        'last_name should be admin')
+                        'last_name should be tester')
         self.assertTrue(type(new_user.last_name) is str,
                         'last_name should be a string')
 
-        del data['last_name']
+        del self.data['last_name']
         del new_user
 
         # Tests User creation without a last_name attr
         with self.assertRaises(ValueError, msg='last_name missing'):
-            User(**data)
+            User(**self.data)
 
-        data['last_name'] = 250
+        self.data['last_name'] = 250
         # Tests User creation with last_name as an integer
         with self.assertRaises(TypeError, msg='last_name must be a string'):
-            User(**data)
+            User(**self.data)
+
+    def test_email(self):
+        """ Tests the email attribute """
+        new_user = User(**self.data)
+        self.assertTrue(new_user.email == self.data.get('email'),
+                        'email should be admin1@example.com')
+        self.assertTrue(isinstance(new_user.email, str),
+                        'email should be a string')
+
+        del new_user
+
+        # Tests User creation with email as an int
+        self.data['email'] = 200
+        with self.assertRaises(TypeError, msg='email must be a string'):
+            User(**self.data)
+
+        # Tests User creation without an email
+        del self.data['email']
+        with self.assertRaises(ValueError, msg='email is required'):
+            User(**self.data)
