936a359d722d7198f7eb76cd854bd86c4ee50735
ubonisrael <akpanudohubonisrael@gmail.com>
2024-08-21 11:13:55 +0100

    Comment model - overwrote id attribute to integer added attribute for author id added path attribute added level method and overwote save method

--- models/comment.py
+++ models/comment.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
 """ The Comment Model """
 
-from sqlalchemy import Column, ForeignKey, String, Text
+from sqlalchemy import Column, ForeignKey, String, Text, INTEGER
 from sqlalchemy.orm import relationship, backref
 from models.base import BaseClass, Base
 
 
 class Comment(BaseClass, Base):
     """ Schema for the Comment class """
+    _N = 6
+
     __tablename__ = 'comments'
-    id = Column(String(60), primary_key=True)
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
     post_id = Column(String(60), ForeignKey('posts.id'), nullable=False)
+    # added column for author of comment
+    author_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     parent_id = Column(String(60), ForeignKey('comments.id'), nullable=True)
     content = Column(Text, nullable=False)
+    # used to track the level of comment and make querying easier
+    path = Column(Text, index=True)
     replies = relationship('Comment',
                            backref=backref('parent', remote_side=[id]),
                            cascade='all, delete-orphan')
+    author = relationship('User', backref=backref('comments', cascade='all, delete-orphan'))
+    
+    def save(self):
+        """updates the attribute 'updated_at' with the current datetime"""
+        super().save()
+        if not self.path:
+            from utils import db
+            prefix = self.parent.path + '.' if self.parent else ''
+            self.path = prefix + '{:0{}d}'.format(self.id, self._N)
+            db.save()
+
+    def level(self):
+        return len(self.path)
