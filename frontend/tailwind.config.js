61934d5a2834e6c7ffc0443b547627bd616aba6c
ubonisrael <akpanudohubonisrael@gmail.com>
2024-08-21 17:24:15 +0100

    added custom colors, iconify plugin and darkMode selector

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,28 @@
+import { addDynamicIconSelectors } from '@iconify/tailwind';
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        'arsenic': '#353D46',
+        'dark-navy-blue': '#13141F',
+        'eerie-black': '#181925',
+        'lightGray': '#f4f4f4'
+      },
       fontFamily: {
         sans: ['Open Sans', 'sans-serif'],
       }
     },
   },
-  plugins: [],
+  plugins: [
+    // Iconify plugin
+    addDynamicIconSelectors(),
+  ],
 }
 
