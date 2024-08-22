2c18e15472b7809657bf399163231824cb436f5f
ubonisrael <akpanudohubonisrael@gmail.com>
2024-08-22 09:40:34 +0100

    added signup and login route to router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import GeneralLayout from './ui/general-layout'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./index.css";
+import GeneralLayout from "./ui/general-layout";
+import Login from "./routes/login/login";
+import { Toaster } from "react-hot-toast";
+import SignUp from "./routes/signup/signup";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <GeneralLayout />,
-    errorElement: <div>Oops, error</div>
-  }
-])
+    errorElement: <div>Oops, error</div>,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        containerClassName="overflow-auto"
+      />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
