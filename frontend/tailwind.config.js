a6b74c199b73f98cd925b04163a3ee5d4320c8fb
ubonisrael <akpanudohubonisrael@gmail.com>
2024-08-23 14:06:13 +0100

    added custom colors (styles)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,7 +13,8 @@ export default {
         'arsenic': '#353D46',
         'dark-navy-blue': '#13141F',
         'eerie-black': '#181925',
-        'lightGray': '#f4f4f4'
+        'lightGray': '#f4f4f4',
+        'gunmetal': '#272839'
       },
       fontFamily: {
         sans: ['Open Sans', 'sans-serif'],
