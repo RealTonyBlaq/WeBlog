5ce953ae6aa788c8d9028ab1ec796952af89f54d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-28 23:02:00 +0100

    pycodestyle fix

--- utils/token.py
+++ utils/token.py
@@ -6,7 +6,8 @@ def generate_confirmation_token(email):
     """Generates a confirmation token"""
     from api.v1.app import app
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
-    return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+    return serializer.dumps(email,
+                            salt=app.config['SECURITY_PASSWORD_SALT'])
 
 
 def confirm_token(token, expiration=86400):
@@ -18,6 +19,6 @@ def confirm_token(token, expiration=86400):
             token, salt=app.config['SECURITY_PASSWORD_SALT'],
             max_age=expiration
         )
-    except:
+    except Exception:
         return False
     return email
