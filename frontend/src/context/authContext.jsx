928bb934e31f1fbee407085e9f81a429c6d776a5
ubonisrael <akpanudohubonisrael@gmail.com>
2024-08-28 11:03:52 +0100

    fixed useeffect bug

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -7,24 +7,18 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-     // fetch user data
-     (async () => {
-        try {
-          const data = await getProfile();
-        setUser(data.user);
-        } catch (e) {
-          console.log(e);
-          setUser(null)
-        }
-      })();
-  }, [])
+    if (!user && "user" in localStorage) {
+      setUser(JSON.parse(localStorage.getItem("user")));
+    } else {
+      localStorage.setItem("user", JSON.stringify(user));
+    }
+  }, [user]);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       {children}
     </AuthContext.Provider>
   );
-
 };
 
 export default AuthProvider;
