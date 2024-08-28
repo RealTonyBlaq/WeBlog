3ec29f46a5bbb231e4cad150251e4aff00a8f6ab
ubonisrael <akpanudohubonisrael@gmail.com>
2024-08-28 10:57:29 +0100

    onsubmit event now triggers a get to the search route

--- frontend/src/ui/search-bar.jsx
+++ frontend/src/ui/search-bar.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function SearchBar() {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => setSearch(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    navigate(`/search?q=${search}`);
+  }
   return (
-    <form action="" className="relative w-full">
+    <form className="relative w-full">
       <input
         id="search"
         name="search"
         type="text"
-        placeholder="Search for posts..."
+        value={search}
+        onChange={handleChange}
+        placeholder="Search for posts by title..."
         className="w-full px-4 py-2 text-sm outline-none rounded-xl sm:rounded-2xl dark:bg-dark-navy-blue/50 dark:text-white border sm:border border-arsenic/50 dark:border-white/50"
       />
       <button
+        onClick={handleSubmit}
         aria-label="search button"
         type="submit"
         className="absolute top-1 sm:top-0 right-0 py-2 pr-4 text-lg text-arsenic dark:text-white"
