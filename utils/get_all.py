bd4c976eb756389b8c3b4ed32f41d6fd3ca7230c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-08-28 23:01:57 +0100

    pycodestyle fix

--- utils/get_all.py
+++ utils/get_all.py
@@ -2,12 +2,13 @@
 import math
 from sqlalchemy import func
 
+
 def get_model_instances(model, filter=None, filter_id=None):
     """ Fetches and returns instances of a model """
     from api.v1.app import executor
     from flask import request
     from utils import db
-    
+
     # get page number
     page = request.args.get('page', type=int, default=1)
     # get limit number
@@ -16,7 +17,7 @@ def get_model_instances(model, filter=None, filter_id=None):
     if page < 1 or limit < 1:
         return ({'message': 'Page number\
                         or limit should not be less than 1'}, 400)
-    
+
     # calculate start and end
     start = limit * (page - 1)
 
@@ -35,7 +36,8 @@ def get_model_instances(model, filter=None, filter_id=None):
     # total_count_subquery = db.query(func.count(model.id)).scalar_subquery()
 
     # Main query to fetch paginated tags and total count
-    # objs_query = db.query(model, total_count_subquery.label('total_count')).offset(start).limit(limit)
+    # objs_query = db.query(model, total_count_subquery.label(
+    # 'total_count')).offset(start).limit(limit)
     # objs = objs_query.all()
     # count = objs[0].total_count if objs else 0
 
@@ -45,7 +47,8 @@ def get_model_instances(model, filter=None, filter_id=None):
     for obj in objs:
         new_obj = obj.to_dict()
         if new_obj.get('__class__') == 'Post':
-            new_obj['author'] = f"{obj.author.first_name} {obj.author.last_name}"
+            new_obj['author'] = f"{obj.author.first_name} \
+                {obj.author.last_name}"
             new_obj['author_avatar'] = obj.author.avatar_url
             new_obj['no_of_comments'] = len(obj.comments)
             new_obj['no_of_likes'] = len(obj.bookmarked_by)
@@ -58,4 +61,4 @@ def get_model_instances(model, filter=None, filter_id=None):
         return ({'message': 'Page out of range'}, 404)
 
     return ({'data': objs_list, 'page': f'{page}',
-                    'total_pages': f"{total_pages}"}, 200)
+             'total_pages': f"{total_pages}"}, 200)
