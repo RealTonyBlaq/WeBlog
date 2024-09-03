a95197b5ae7d8e1173eaa321fb90268415922255
ubonisrael <akpanudohubonisrael@gmail.com>
2024-09-03 12:35:24 +0100

    rendered correct UI

--- frontend/src/routes/resend_confirmation_email/resend_confirmation_email.jsx
+++ frontend/src/routes/resend_confirmation_email/resend_confirmation_email.jsx
@@ -1,7 +1,7 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Bars } from "react-loader-spinner";
 import { Form, Formik } from "formik";
-import { resetPasswordSchema } from "../../lib/utils";
+import { forgotPasswordSchema } from "../../lib/utils";
 import { MyTextInput } from "../../ui/form-input";
 import { useResendConfirmationEmail } from "../../api/auth";
 
@@ -9,7 +9,6 @@ export default function ResendConfirmationEmail() {
   const { mutate, isLoading } = useResendConfirmationEmail();
 
   const submitFormHandler = async (values) => {
-    console.log(values);
     mutate({
       email: values.email,
     });
@@ -19,15 +18,14 @@ export default function ResendConfirmationEmail() {
     <div className="w-full h-[calc(100vh-128px)] sm:h-[calc(100vh-144px)] md:h-[calc(100vh-152px)] flex items-center justify-center">
       <div className="w-[90%] max-w-md mx-auto grid p-4 md:p-6 xl:p-10 rounded-3xl shadow-lg bg-white dark:bg-dark-navy-blue">
         <p className="py-4 md:py-6 lg:py-8 text-2xl md:text-3xl xl:text-4xl text-center font-bold text-arsenic dark:text-white">
-          Reset Password
+          Resend Confirmation Email
         </p>
         <div className="w-full">
           <Formik
             initialValues={{
-              password: "",
-              confirm_password: "",
+              email: "",
             }}
-            validationSchema={resetPasswordSchema}
+            validationSchema={forgotPasswordSchema}
             onSubmit={(values, { setSubmitting }) => {
               submitFormHandler(values);
               setSubmitting(false);
@@ -37,14 +35,9 @@ export default function ResendConfirmationEmail() {
               return (
                 <Form className="w-full mb-4 md:mb-8 max-w-xs mx-auto flex flex-col gap-3 md:gap-4 items-center">
                   <MyTextInput
-                    label="Password"
-                    name="password"
-                    type="password"
-                  />
-                  <MyTextInput
-                    label="Confirm Password"
-                    name="confirm_password"
-                    type="password"
+                    label="Email"
+                    name="email"
+                    type="text"
                   />
                   <button
                     type="submit"
@@ -61,7 +54,7 @@ export default function ResendConfirmationEmail() {
                         color="white"
                       />
                     ) : (
-                      "Reset Password"
+                      "Resend"
                     )}
                   </button>
                 </Form>
