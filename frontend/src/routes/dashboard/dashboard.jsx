3521f81fdaf23813446e4d8150bcfc761578d38a
ubonisrael <akpanudohubonisrael@gmail.com>
2024-09-05 18:36:23 +0100

    added link for my drafts page modified CSS styles for nav links

--- frontend/src/routes/dashboard/dashboard.jsx
+++ frontend/src/routes/dashboard/dashboard.jsx
@@ -1,17 +1,23 @@
-import { NavLink, Outlet, redirect, useNavigate } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "../../lib/useAuth";
 import toast from "react-hot-toast";
 import { useEffect } from "react";
 
 const links = [
-
   {
     url: "/my_posts",
     name: "My Posts",
+    icon: <span className="icon-[ooui--articles-rtl]"></span>
+  },
+  {
+    url: "/my_drafts",
+    name: "My Drafts",
+    icon: <span className="icon-[fluent--drafts-20-filled]"></span>
   },
   {
     url: "/my_bookmarks",
     name: "My Bookmarks",
+    icon: <span className="icon-[ion--bookmarks-sharp]"></span>
   },
   // {
   //   url: "/my_tags",
@@ -28,30 +34,31 @@ export default function DashBoard() {
       toast.error('Please login to access your dashboard')
       navigate('/login')
     }
-  }, [])
+  }, [user])
+
   return (
-    <div className="w-full grid md:grid-cols-[1fr_2fr] xl:grid-cols-[1fr_3fr] gap-4 sm:gap-6 md:gap-8 xl:gap-12 p-4 sm:px-8 md:py-8 md:px-12 lg:px-20 xl:py-12 xl:px-24">
-      <nav className="w-full md:h-56 p-4 md:p-6 rounded-lg bg-white dark:bg-dark-navy-blue text-arsenic dark:text-white">
-        <ul className="w-full h-full grid grid-cols-2 md:block">
+    <div className="w-full h-[calc(100vh-128px)] sm:h-[calc(100vh-144px)] md:h-[calc(100vh-152px)] grid grid-cols-[2fr_9fr] xl:grid-cols-[1fr_4fr] lg:gap-8 xl:gap-12 p-4 md:py-8 md:px-8 lg:px-20 xl:py-12 xl:px-24">
+      <nav className="w-full md:h-56 text-arsenic dark:text-white">
+        <ul className="w-full h-full flex flex-col items-center md:items-start justify-center gap-2">
           {links.map((link) => (
             <li key={link.name} className="mb-1 md:mb-2">
               <NavLink
                 to={`/dashboard${link.url}`}
                 className={({ isActive, isPending }) =>
                   isActive
-                    ? "text-arsenic bg-slate-100 p-2 w-full block font-semibold rounded hover:bg-arsenic hover:text-white dark:hover:bg-white dark:hover:text-arsenic"
+                    ? "text-black dark:text-white bg-white dark:bg-black p-2 w-full flex items-center gap-1 md:gap-2 font-bold rounded hover:bg-arsenic hover:text-white dark:hover:bg-white dark:hover:text-arsenic shadow"
                     : isPending
                     ? "pending"
-                    : "p-2 w-full block font-medium rounded hover:bg-arsenic hover:text-white dark:hover:bg-white dark:hover:text-arsenic"
+                    : "p-2 w-full flex items-center gap-1 md:gap-2 font-medium rounded hover:bg-arsenic hover:text-white dark:hover:bg-white dark:hover:text-arsenic"
                 }
               >
-                {link.name}
+                {link.icon} <span className="hidden md:block">{link.name}</span>
               </NavLink>
             </li>
           ))}
         </ul>
       </nav>
-      <div className="w-full px-6 py-4 md:p-0">
+      <div className="w-full h-full overflow-y-scroll px-6 py-4 md:p-0">
         <Outlet />
       </div>
     </div>
