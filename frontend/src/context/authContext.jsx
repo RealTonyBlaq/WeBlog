639271b3ee68a5e42610629c8253723a30a58417
ubonisrael <akpanudohubonisrael@gmail.com>
2024-09-06 13:10:05 +0100

    ensured user object if logged in is not null on initial render

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect } from "react";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 
   useEffect(() => {
     if (!user && "user" in localStorage) {
