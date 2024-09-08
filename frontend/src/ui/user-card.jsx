0d6a9304cd1fde3c8da76e179a3542e6369515b7
ubonisrael <akpanudohubonisrael@gmail.com>
2024-09-08 08:43:48 +0100

    modified CSS styles for usercard component

--- frontend/src/ui/user-card.jsx
+++ frontend/src/ui/user-card.jsx
@@ -2,10 +2,9 @@ import { Link } from "react-router-dom";
 import Avatar from "./avatar";
 
 export default function UserCard({ user }) {
-    console.log(user);
     
   return (
-    <Link to={""} className="w-full grid grid-cols-[1fr_4fr_6fr_1fr_1.5fr] items-center gap-2 md:gap-3 bg-white hover:bg-slate-100 dark:bg-dark-navy-blue dark:hover:bg-black p-2 md:p-4 rounded-md shadow">
+    <Link to={`/admin/users/${user.id}`} className="w-full grid grid-cols-[1fr_4fr_6fr_1fr_1.5fr] items-center gap-2 md:gap-3 bg-white hover:bg-slate-100 dark:bg-dark-navy-blue dark:hover:bg-black p-2 md:p-4 rounded-md shadow">
       <Avatar author={user} />
       <p className="font-medium">{user.first_name + " " + user.last_name}</p>
       <p>{user.email}</p>
