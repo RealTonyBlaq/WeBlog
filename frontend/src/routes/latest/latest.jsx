5018e8077bc1aaa2590b94376eac854619ab2c79
ubonisrael <akpanudohubonisrael@gmail.com>
2024-09-09 21:58:24 +0100

    reorganized LatestPostsPage to be the home page

--- frontend/src/routes/latest/latest.jsx
+++ frontend/src/routes/latest/latest.jsx
@@ -5,5 +5,9 @@ import DisplayPosts from "../../ui/display-posts";
 export default function LatestPostsPage() {
   const { posts } = useLoaderData();
 
-  return <DisplayPosts posts={posts} fetchData={fetchPosts} />;
+  return (
+    <div className="w-full max-w-2xl xl:max-w-4xl mx-auto p-4 sm:px-8 md:py-4 md:px-12 lg:px-20 xl:py-12 xl:px-24">
+      <DisplayPosts posts={posts} fetchData={fetchPosts} />
+    </div>
+  );
 }
