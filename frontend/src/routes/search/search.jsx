76d3b541596188c9c8a5fcfe11150f9f5246bb03
ubonisrael <akpanudohubonisrael@gmail.com>
2024-09-10 13:40:02 +0100

    improved UI layout by reorganizing CSS styles

--- frontend/src/routes/search/search.jsx
+++ frontend/src/routes/search/search.jsx
@@ -5,5 +5,9 @@ import DisplayPosts from "../../ui/display-posts";
 export default function QueryPostsPage() {
   const { posts } = useLoaderData();
 
-  return <DisplayPosts posts={posts} fetchData={queryPosts} />;
+  return (
+    <div className="w-full max-w-2xl xl:max-w-4xl mx-auto p-4 sm:px-8 md:py-4 md:px-12 lg:px-20 xl:py-12 xl:px-24">
+      <DisplayPosts posts={posts} fetchData={queryPosts} />
+    </div>
+  );
 }
