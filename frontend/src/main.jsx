6f206d0ac4d1e6a7a05481adf478987c3fbd4a03
ubonisrael <akpanudohubonisrael@gmail.com>
2024-09-10 13:44:24 +0100

    feature - used dynamic loading to reduce js bundle size

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  redirect,
   Route,
   RouterProvider,
 } from "react-router-dom";
@@ -11,206 +12,28 @@ import "./index.css";
 import { Toaster } from "react-hot-toast";
 // pages
 import GeneralLayout from "./ui/general-layout";
-import Login from "./routes/login/login";
-import SignUp from "./routes/signup/signup";
-import ResetPassword from "./routes/reset_password/reset_password";
-import ForgotPassword from "./routes/forgot_password/forgot_password";
-import ResendConfirmationEmail from "./routes/resend_confirmation_email/resend_confirmation_email";
 import AuthProvider from "./context/authContext";
-import DashBoard from "./routes/dashboard/dashboard";
-import MyPostsPage from "./routes/my_posts/my_posts";
-import MyBookmarksPage from "./routes/my_bookmarks/my_bookmarks";
-import MyProfilePage from "./routes/my_profile/my_profile";
-import EditMyProfile from "./routes/edit_profile/edit_profile";
-import ChangeMyPassword from "./routes/change_password/change_pasword";
-import CreatePostPage from "./routes/create_posts/create_post";
-// import HomePage from "./routes/home/home";
 import LatestPostsPage from "./routes/latest/latest";
-import RenderPostMarkDown from "./routes/post/post";
-import EditPostPage from "./routes/edit_posts/edit_posts";
 import ErrorPage from "./error-page";
-import QueryPostsPage from "./routes/search/search";
-import MyDraftsPage from "./routes/my_drafts/my_drafts";
-import AdminDashboard from "./routes/admin/admin";
-import AdminTagsPage from "./routes/admin_tags/admin_tags";
-import CreateTagPage from "./routes/create_tag/create_tag";
-import TagPage from "./routes/tag/tag";
-import EditTagPage from "./routes/edit_tag/edit_tag";
-import AdminUsersPage from "./routes/admin_users/admin_users";
-import UserPage from "./routes/user/user";
-import AdminPostsPage from "./routes/admin_posts/admin_posts";
 import NotFoundPage from "./404";
 // loaders
 import { loader as HomeLoader } from "./routes/home/loader";
-import { loader as PostLoader } from "./routes/post/loader";
-import { loader as TagsLoader } from "./routes/create_posts/loader";
-import { loader as myPostsLoader } from "./routes/my_posts/loader";
-import { loader as myDraftsLoader } from "./routes/my_drafts/loader";
-import { loader as editPostLoader } from "./routes/edit_posts/loader";
-import { loader as bookmarksLoader } from "./routes/my_bookmarks/loader";
-import { loader as searchLoader } from "./routes/search/loader";
-import { loader as tagDataLoader } from "./routes/tag/loader";
-import { loader as tagsDataLoader } from "./routes/admin_tags/loader";
-import { loader as editTagDataLoader } from "./routes/edit_tag/loader";
-import { loader as userDataLoader } from "./routes/user/loader";
-import { loader as usersDataLoader } from "./routes/admin_users/loader";
-// actions
-import { action as DeletePost } from "./routes/delete_post/action";
+// api functions
+import {
+  deleteMyPost,
+  fetchMyBookmarks,
+  fetchMyDrafts,
+  fetchMyPost,
+  fetchMyPosts,
+  fetchPost,
+  fetchPosts,
+  queryPosts,
+} from "./api/posts";
+import { fetchPostComments } from "./api/comments";
+import { fetchUser, fetchUserPosts, fetchUsers } from "./api/users";
+import { fetchTag, fetchTagPosts, fetchTags } from "./api/tags";
 
 const queryClient = new QueryClient();
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: (
-//       <AuthProvider>
-//         <GeneralLayout />
-//       </AuthProvider>
-//     ),
-//     errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <HomePage />,
-//         children: [
-//           {
-//             errorElement: <div>Oops....Page not found</div>,
-//             children: [
-//               {
-//                 index: true,
-//                 element: <LatestPostsPage />,
-//                 loader: HomeLoader,
-//               },
-//             ],
-//           },
-//         ],
-//       },
-//       {
-//         path: "search",
-//         element: <QueryPostsPage />,
-//         loader: searchLoader
-//       },
-//       {
-//         path: "posts/:id",
-//         element: <RenderPostMarkDown />,
-//         loader: PostLoader,
-//       },
-//       {
-//         path: "login",
-//         element: <Login />,
-//       },
-//       {
-//         path: "signup",
-//         element: <SignUp />,
-//       },
-//       {
-//         path: "forgot-password",
-//         element: <ForgotPassword />,
-//       },
-//       {
-//         path: "reset_password/:token/:user_id",
-//         element: <ResetPassword />,
-//       },
-//       {
-//         path: "resend_conf_email",
-//         element: <ResendConfirmationEmail />,
-//       },
-//       {
-//         path: "admin",
-//         element: <AdminDashboard />,
-//         children: [
-//           {
-//             path: 'users',
-//             element: <AdminUsersPage />,
-//           },
-//           {
-//             path: 'users/:id',
-//             element: <UserPage />,
-//             loader: userDataLoader
-//           },
-//           {
-//             path: 'articles',
-//             element: <AdminPostsPage />
-//           },
-//           {
-//             path: 'tags',
-//             element: <AdminTagsPage />,
-//           },
-//           {
-//             path: 'tags/:id',
-//             element: <TagPage />,
-//             loader: tagDataLoader
-//           },
-//           {
-//             path: 'tags/:id/edit',
-//             element: <EditTagPage />,
-//             loader: editTagDataLoader
-//           },
-//           {
-//             path: 'create_tag',
-//             element: <CreateTagPage />,
-//           }
-//         ]
-//       },
-//       {
-//         path: "dashboard",
-//         element: <DashBoard />,
-//         children: [
-//           {
-//             errorElement: <ErrorPage />,
-//             children: [
-//               {
-//                 index: true,
-//                 element: <MyProfilePage />,
-//               },
-//               {
-//                 path: "create_post",
-//                 element: <CreatePostPage />,
-//                 loader: TagsLoader
-//               },
-//               {
-//                 path: "edit_profile",
-//                 element: <EditMyProfile />,
-//               },
-//               {
-//                 path: "change_password",
-//                 element: <ChangeMyPassword />,
-//               },
-//               {
-//                 path: "my_posts",
-//                 element: <MyPostsPage />,
-//                 loader: myPostsLoader
-//               },
-//               {
-//                 path: "my_drafts",
-//                 element: <MyDraftsPage />,
-//                 loader: myDraftsLoader
-//               },
-//               {
-//                 path: "my_posts/:id/delete",
-//                 action: DeletePost,
-//               },
-//               {
-//                 path: "my_posts/:id/edit",
-//                 element: <EditPostPage />,
-//                 loader: editPostLoader
-//               },
-//               {
-//                 path: "my_bookmarks",
-//                 element: <MyBookmarksPage />,
-//                 loader: bookmarksLoader
-//               },
-//             ],
-//           },
-//         ],
-//       },
-//       {
-//         path: "*",
-//         element: <NotFoundPage />
-//       }
-//     ],
-//   },
-// ]);
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -225,69 +48,287 @@ const router = createBrowserRouter(
     >
       <Route errorElement={<ErrorPage />}>
         <Route index element={<LatestPostsPage />} loader={HomeLoader} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="forgot-password" element={<ForgotPassword />} />
-        <Route path="reset-password" element={<ResetPassword />} />
-        <Route path="resend_conf_email" element={<ResendConfirmationEmail />} />
+        <Route
+          path="login"
+          lazy={async () => {
+            let Login = await import("./routes/login/login");
+            return { Component: Login.default };
+          }}
+        />
+        <Route
+          path="signup"
+          lazy={async () => {
+            let Signup = await import("./routes/signup/signup");
+            return { Component: Signup.default };
+          }}
+        />
+        <Route
+          path="forgot-password"
+          lazy={async () => {
+            let ForgotPassword = await import(
+              "./routes/forgot_password/forgot_password"
+            );
+            return { Component: ForgotPassword.default };
+          }}
+        />
+        <Route
+          path="reset-password"
+          lazy={async () => {
+            let ResetPassword = await import(
+              "./routes/reset_password/reset_password"
+            );
+            return { Component: ResetPassword.default };
+          }}
+        />
+        <Route
+          path="resend_conf_email"
+          lazy={async () => {
+            let ResendConfirmationEmail = await import(
+              "./routes/resend_confirmation_email/resend_confirmation_email"
+            );
+            return { Component: ResendConfirmationEmail.default };
+          }}
+        />
         <Route
           path="search"
-          element={<QueryPostsPage />}
-          loader={searchLoader}
+          lazy={async () => {
+            let QueryPostsPage = await import("./routes/search/search");
+            return { Component: QueryPostsPage.default };
+          }}
+          loader={async ({ request }) => {
+            const url = new URL(request.url);
+            const q = url.searchParams.get("q") || "";
+            const posts = await queryPosts(q);
+
+            return { posts };
+          }}
         />
         <Route
           path="posts/:id"
-          element={<RenderPostMarkDown />}
-          loader={PostLoader}
+          lazy={async () => {
+            let RenderPostMarkDown = await import("./routes/post/post");
+            return { Component: RenderPostMarkDown.default };
+          }}
+          loader={async ({ params }) => {
+            const post = await fetchPost(params.id);
+            const comments = await fetchPostComments(params.id);
+
+            return { post, comments: comments.data };
+          }}
         />
-        <Route path="admin" element={<AdminDashboard />}>
-          <Route path="users" element={<AdminUsersPage />} loader={usersDataLoader} />
+        <Route
+          path="admin"
+          lazy={async () => {
+            let AdminDashboard = await import("./routes/admin/admin");
+            return { Component: AdminDashboard.default };
+          }}
+        >
+          <Route
+            path="users"
+            lazy={async () => {
+              let AdminUsersPage = await import(
+                "./routes/admin_users/admin_users"
+              );
+              return { Component: AdminUsersPage.default };
+            }}
+            loader={async () => {
+              const { data } = await fetchUsers();
+
+              return { data };
+            }}
+          />
           <Route
             path="users/:id"
-            element={<UserPage />}
-            loader={userDataLoader}
+            lazy={async () => {
+              let UserPage = await import("./routes/user/user");
+              return { Component: UserPage.default };
+            }}
+            loader={async ({ params }) => {
+              const {
+                data: { user },
+              } = await fetchUser(params.id);
+              const { data: postsData } = await fetchUserPosts(params.id);
+
+              return { user, postsData };
+            }}
           />
-          <Route path="articles" element={<AdminPostsPage />} />
-          <Route path="tags" element={<AdminTagsPage />} loader={tagsDataLoader} />
-          <Route path="tags/:id" element={<TagPage />} loader={tagDataLoader} />
+          <Route
+            path="articles"
+            lazy={async () => {
+              let AdminPostsPage = await import(
+                "./routes/admin_posts/admin_posts"
+              );
+              return { Component: AdminPostsPage.default };
+            }}
+            loader={async () => {
+              const { data, page, total_pages } = await fetchPosts();
+
+              return { data, page, total_pages };
+            }}
+          />
+          <Route
+            path="tags"
+            lazy={async () => {
+              let AdminTagsPage = await import(
+                "./routes/admin_tags/admin_tags"
+              );
+              return { Component: AdminTagsPage.default };
+            }}
+            loader={async () => {
+              const { data } = await fetchTags();
+
+              return { data };
+            }}
+          />
+          <Route
+            path="tags/:id"
+            lazy={async () => {
+              let TagPage = await import("./routes/tag/tag");
+              return { Component: TagPage.default };
+            }}
+            loader={async ({ params }) => {
+              const {
+                data: { tag },
+              } = await fetchTag(params.id);
+              const { data: postsData } = await fetchTagPosts(params.id);
+
+              return { tag, postsData };
+            }}
+          />
           <Route
             path="tags/:id/edit"
-            element={<EditTagPage />}
-            loader={editTagDataLoader}
+            lazy={async () => {
+              let EditTagPage = await import("./routes/edit_tag/edit_tag");
+              return { Component: EditTagPage.default };
+            }}
+            loader={async ({ params }) => {
+              const {
+                data: { tag },
+              } = await fetchTag(params.id);
+
+              return { tag };
+            }}
           />
-          <Route path="create_tag" element={<CreateTagPage />} />
+          <Route
+            path="create_tag"
+            lazy={async () => {
+              let CreateTagPage = await import(
+                "./routes/create_tag/create_tag"
+              );
+              return { Component: CreateTagPage.default };
+            }}
+          />
         </Route>
-        <Route path="dashboard" element={<DashBoard />}>
+        <Route
+          path="dashboard"
+          lazy={async () => {
+            let DashBoard = await import("./routes/dashboard/dashboard");
+            return { Component: DashBoard.default };
+          }}
+        >
           <Route errorElement={<ErrorPage />}>
-            <Route index element={<MyProfilePage />} />
+            <Route
+              index
+              lazy={async () => {
+                let MyProfilePage = await import(
+                  "./routes/my_profile/my_profile"
+                );
+                return { Component: MyProfilePage.default };
+              }}
+            />
             <Route
               path="create_post"
-              element={<CreatePostPage />}
-              loader={TagsLoader}
+              lazy={async () => {
+                let CreatePostPage = await import(
+                  "./routes/create_posts/create_post"
+                );
+                return { Component: CreatePostPage.default };
+              }}
+              loader={async () => {
+                const {
+                  data: { tags },
+                } = await fetchTags();
+
+                return { tags };
+              }}
             />
-            <Route path="edit_profile" element={<EditMyProfile />} />
-            <Route path="change_password" element={<ChangeMyPassword />} />
+            <Route
+              path="edit_profile"
+              lazy={async () => {
+                let EditMyProfile = await import(
+                  "./routes/edit_profile/edit_profile"
+                );
+                return { Component: EditMyProfile.default };
+              }}
+            />
+            <Route
+              path="change_password"
+              lazy={async () => {
+                let ChangeMyPassword = await import(
+                  "./routes/change_password/change_pasword"
+                );
+                return { Component: ChangeMyPassword.default };
+              }}
+            />
             <Route
               path="my_bookmarks"
-              element={<MyBookmarksPage />}
-              loader={bookmarksLoader}
+              lazy={async () => {
+                let MyBookmarksPage = await import(
+                  "./routes/my_bookmarks/my_bookmarks"
+                );
+                return { Component: MyBookmarksPage.default };
+              }}
+              loader={async () => {
+                const postsData = await fetchMyBookmarks();
+                return postsData;
+              }}
             />
             <Route
               path="my_drafts"
-              element={<MyDraftsPage />}
-              loader={myDraftsLoader}
+              lazy={async () => {
+                let MyDraftsPage = await import("./routes/my_drafts/my_drafts");
+                return { Component: MyDraftsPage.default };
+              }}
+              loader={async () => {
+                const postsData = await fetchMyDrafts();
+                return postsData;
+              }}
             />
             <Route
               path="my_posts"
-              element={<MyPostsPage />}
-              loader={myPostsLoader}
+              lazy={async () => {
+                let MyPostsPage = await import("./routes/my_posts/my_posts");
+                return { Component: MyPostsPage.default };
+              }}
+              loader={async () => {
+                const postsData = await fetchMyPosts();
+
+                return postsData;
+              }}
             />
             <Route
               path="my_posts/:id/edit"
-              element={<EditPostPage />}
-              loader={editPostLoader}
+              lazy={async () => {
+                let EditPostPage = await import(
+                  "./routes/edit_posts/edit_posts"
+                );
+                return { Component: EditPostPage.default };
+              }}
+              loader={async ({ params }) => {
+                const {
+                  data: { tags },
+                } = await fetchTags();
+                const { data: post } = await fetchMyPost(params.id);
+                return { tags, post };
+              }}
             />
-            <Route path="my_posts/:id/delete" action={DeletePost} />
+            <Route
+              path="my_posts/:id/delete"
+              action={async ({ params }) => {
+                await deleteMyPost(params.id);
+                return redirect(`/dashboard/my_posts`);
+              }}
+            />
           </Route>
         </Route>
         <Route path="*" element={<NotFoundPage />} />
