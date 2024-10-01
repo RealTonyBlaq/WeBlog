027f4a0e7cbb10f48d035f49e35f4baeb51bd762
Ubonisrael Akpanudoh <123631451+ubonisrael@users.noreply.github.com>
2024-10-01 16:01:53 +0100

    Update footer.jsx

--- frontend/src/ui/footer.jsx
+++ frontend/src/ui/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="sticky top-0 left-0 w-full px-6 py-4 sm:px-8 sm:py-6 md:px-12 lg:px-20 xl:px-24 bg-slate-200 dark:bg-black text-black dark:text-white">
+    <footer className="w-full px-6 py-4 sm:px-8 sm:py-6 md:px-12 lg:px-20 xl:px-24 bg-slate-200 dark:bg-black text-black dark:text-white">
       <p className="text-center text-arsenic dark:text-white font-medium">
         Made with ❤️ and{" "}
         <a target="_blank" href="https://flask.palletsprojects.com/en/3.0.x/">
