03a2b3417fc7468297dba19e7723eca276d7c5bc
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-11 22:38:21 +0100

    test database

--- tests/test_utils/test_database.py
+++ tests/test_utils/test_database.py
@@ -1,1 +1,10 @@
-#!/
+#!/usr/bin/env python3
+""" Test Suite for the Database """
+
+from unittest import TestCase
+from utils.database import Storage
+
+
+class Test_Storage(TestCase):
+    """ Tests the Storage class """
+    pass
